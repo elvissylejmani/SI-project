ee693b590a7b778a278366fe7105d3d18dae5566
elvis sylejmani <elvissylejmani13@gmail.com>
2020-05-06 01:16:48 +0200

    guest&auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,17 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                        @guest
+                           <p style="color: red"> Kjo e dhënë nuk mund të shihet nga vizitorët e pa log-our </p>
+                        @endguest
 
-                    You are logged in!
+                        @auth
+                       <p style="color: aqua"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat n
+                        on proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                        @endauth
                 </div>
             </div>
         </div>
