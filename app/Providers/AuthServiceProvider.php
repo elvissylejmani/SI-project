2ebef2c14acad76e09c8ff7862c89b76519d6966
elvis sylejmani <elvissylejmani13@gmail.com>
2020-05-06 05:07:13 +0200

    gate name changed

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,7 +25,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Gate::define('isAdmin', function ($user) {
+        Gate::define('edit-settings', function ($user) {
             return $user->isAdmin;
         });
 
