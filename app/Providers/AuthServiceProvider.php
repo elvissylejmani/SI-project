e71be300e96e66c80f37a940ff1327cfd1f39447
elvis sylejmani <elvissylejmani13@gmail.com>
2020-05-06 05:04:30 +0200

    gate defined

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        Gate::define('edit-settings', function ($user) {
+            return $user->isAdmin;
+        });
+
+
         //
     }
 }
